dc6104307fa29fa43f3095ea5bf045d174ab62b5
shreeyaptl <85817711+shreeyaptl@users.noreply.github.com>
2021-12-21 01:37:15 +0530

    Add files via upload

---
+++ java_array2.java
@@ -0,0 +1,11 @@
+class Main
+    {
+        public static void main(String args[]){
+            int a[] = {10,20,30,40,50};
+            System.out.println("one dimensional array elements are: \n");
+            for(int i=0;i<a.length;i++)
+            {
+                System.out.println("a["+i+"]:"+a[i]);
+            }
+        }
+   }
