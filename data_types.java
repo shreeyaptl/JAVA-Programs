1fbf0be72c70fcc37faf8957715f92c723774ff1
shreeyaptl <85817711+shreeyaptl@users.noreply.github.com>
2022-02-01 00:16:32 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,20 @@
+public class Main {
+    
+    static void myFunc() {
+        System.out.println("HEY THERE!");
+    }
+    static void myStringfunc(String fname)
+    {
+        System.out.println(fname + " " +"patel");
+    }
+    public static void main(String[] args)
+    {
+        int myInt = 9;
+        int myVar = 9 + 10;
+        System.out.println(myVar);
+        String greeting = "Hello world";
+        myFunc();
+        myStringfunc("shreeya");
+    }
+    
+}
